4fbcc17e1dd753972a3daf8d7205c047a513e15e
Kedar Nadkarny <kedar.nadkarny@gmail.com>
2017-05-30 21:19:16 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  employees = [
+    {name:"Kedar", position: "programmer"},
+    {name:"Alex", position: "Designer"},
+    {name:"Maria", position: "manager"}
+  ];
+  model:any={};
+  model2:any={};
+  addEmployee() {
+      this.employees.push(this.model);
+      this.model = {};
+  }
+  deleteEmployee(i) {
+    this.employees.splice(i, 1);
+    console.log(i);
+  }
+  myValue;
+  editEmployee(k) {
+    this.model2.name = this.employees[k].name;
+    this.model2.position = this.employees[k].position;
+    this.myValue = k;
+  }
+  updateEmployee() {
+    let k = this.myValue;
+    for(let i=0; i<this.employees.length; i++){
+      if(i==k) {
+        this.employees[i] = this.model2;
+        this.model2 = {};
+      }
+      console.log(i);
+    }
+  }
 }
